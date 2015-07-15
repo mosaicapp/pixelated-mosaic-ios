4dcbecf60f130c450fed82b5265d46a5d36d00f2
Oliver Becker <ob@obqo.de>
2015-07-15 21:14:56 +0200

    use parameters for GET call

--- Mosaic/ViewController.swift
+++ Mosaic/ViewController.swift
@@ -5,7 +5,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Alamofire.request(.GET, URLString: "http://localhost:3333/mails?q=tag%3A%22inbox%22&p=1&w=25")
+        Alamofire.request(.GET, URLString: "http://localhost:3333/mails",
+                                parameters: ["q": "tag:inbox", "p": 1, "w": 25])
             .responseJSON(completionHandler: { (request, response, json, error) -> Void in
                 print(json)
         })
