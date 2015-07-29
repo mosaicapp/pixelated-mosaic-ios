7a05990a29ec1fef95e78e088822b36bf8223c1d
Hermann Vocke <hvocke@thoughtworks.com>
2015-07-29 22:26:48 +0200

    doing http request and showing emails in table view

--- Mosaic/MainInboxController.swift
+++ Mosaic/MainInboxController.swift
@@ -1,23 +1,45 @@
 import UIKit
+import Alamofire
 
 class MainInboxController : UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var tableView: UITableView!
+    
+    let jsonParseService = JsonParseService()
+    
+    var mailsResponse : MailsResponse?
     
     override func viewDidLoad() {
         tableView.delegate = self
         tableView.dataSource = self
     }
     
+    override func viewWillAppear(animated: Bool) {
+        loadInbox()
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("inboxMailCell") as! InboxMailCell
-        cell.subject.text = "test subject"
-        cell.body.text = "This is the body of my static test email"
-        cell.date.text = "01/01/2015"
+        let cell = tableView.dequeueReusableCellWithIdentifier("inboxMailCell") as! InboxMailCell
+        
+        if let response = mailsResponse {
+            cell.subject.text = response.mails[indexPath.row].header.subject
+            cell.body.text = response.mails[indexPath.row].textPlainBody
+            cell.date.text = "01/01/2015"
+        }
+        
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return mailsResponse == nil ? 0 : mailsResponse!.mails.count
+    }
+    
+    private func loadInbox() {
+        Alamofire.request(.GET, "http://localhost:3333/mails",
+            parameters: ["q": "tag:inbox", "p": 1, "w": 25])
+            .responseJSON(completionHandler: { (request, response, json, error) -> Void in
+                self.mailsResponse = self.jsonParseService.parseMailsResponse(json)
+                self.tableView.reloadData()
+            })
     }
 }
