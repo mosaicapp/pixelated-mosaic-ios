84ddc0c30e88dbdd1e84f0c5745a467716899f68
Rene Lindhorst <rene.lindhorst@lindhorst.it>
2015-07-29 20:10:40 +0200

    Alamofire syntax changes

--- Mosaic/ViewController.swift
+++ Mosaic/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Alamofire.request(.GET, URLString: "http://localhost:3333/mails",
+        Alamofire.request(.GET, "http://localhost:3333/mails",
                                 parameters: ["q": "tag:inbox", "p": 1, "w": 25])
             .responseJSON(completionHandler: { (request, response, json, error) -> Void in
                 print("json: \(json)")
