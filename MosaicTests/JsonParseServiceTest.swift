99697b417d64cbfd9632b77b42aa6f95509c86bb
Oliver Becker <ob@obqo.de>
2015-10-14 12:07:35 +0200

    test refactoring: added two expect functions that wrap the pattern matching code, thus making the test clearer

--- MosaicTests/JsonParseServiceTest.swift
+++ MosaicTests/JsonParseServiceTest.swift
@@ -2,57 +2,65 @@ import Quick
 import Nimble
 import XCTest
 
+private func expectSuccess<T>(result: JsonParseResult<T>, file: String = __FILE__, line: UInt = __LINE__) -> T? {
+    guard case let .Success(t) = result else {
+        fail("expected <Success>, got <\(result)>", file: file, line: line)
+        return nil
+    }
+    return t
+}
+
+private func expectParsed(date: Date, file: String = __FILE__, line: UInt = __LINE__) -> NSDate? {
+    guard case let .Parsed(value) = date else {
+        fail("expected <Parsed>, got <\(date)>", file: file, line: line)
+        return nil
+    }
+    return value
+}
+
 class JsonParseServiceTestSpec: QuickSpec {
+    
     override func spec() {
         
         let jsonParseService = JsonParseService()
         
-        describe("the MailService") {
-            it("should correctly parse JSON response") {
+        describe("the JsonParseService") {
+            it("should correctly parse JSON a response") {
                 let json = JsonFromFile("mailsResponse")
                 let result = jsonParseService.parseMails(json)
                 
-                switch result {
-                case let .Success(res):
-                    expect(res.stats.total) == 2
-                    expect(res.mails.count) == 2
-                    
-                    var mail = res.mails[0]
-                    var header = mail.header
-                    expect(header.from) == "alice@dev.pixelated-project.org"
-                    expect(header.to.count) == 1
-                    expect(header.to).to(contain("bob@dev.pixelated-project.org"))
-                    expect(header.cc!.count) == 2
-                    expect(header.bcc!.count) == 1
-                    expect(header.subject) == "Hello"
-                    if case .Parsed(_) = header.date {
-                        // ok
-                    } else {
-                        XCTFail("date has not been parsed")
-                    }
-                    expect(mail.textPlainBody) == "world"
-                    expect(mail.mailbox) == "inbox"
-                    expect(mail.ident) == "M-xxxa"
-                    
-                    mail = res.mails[1]
-                    header = mail.header
-                    expect(header.from).to(beNil())
-                    expect(header.to.count) == 1
-                    expect(header.to).to(contain("alice@dev.pixelated-project.org"))
-                    expect(header.cc).to(beNil())
-                    expect(header.bcc).to(beNil())
-                    expect(header.subject) == "Welcome"
-                    if case .Parsed(_) = header.date {
-                        // ok
-                    } else {
-                        XCTFail("date has not been parsed")
-                    }
-                    expect(mail.textPlainBody) == "First mail"
-                    expect(mail.mailbox) == "inbox"
-                    expect(mail.ident) == "M-xxxb"
-                case let .Failure(msg):
-                    XCTFail(msg)
+                guard let mails = expectSuccess(result) else {
+                    return
                 }
+                
+                expect(mails.stats.total) == 2
+                expect(mails.mails.count) == 2
+                
+                var mail = mails.mails[0]
+                var header = mail.header
+                expect(header.from) == "alice@dev.pixelated-project.org"
+                expect(header.to.count) == 1
+                expect(header.to).to(contain("bob@dev.pixelated-project.org"))
+                expect(header.cc!.count) == 2
+                expect(header.bcc!.count) == 1
+                expect(header.subject) == "Hello"
+                expectParsed(header.date)
+                expect(mail.textPlainBody) == "world"
+                expect(mail.mailbox) == "inbox"
+                expect(mail.ident) == "M-xxxa"
+                
+                mail = mails.mails[1]
+                header = mail.header
+                expect(header.from).to(beNil())
+                expect(header.to.count) == 1
+                expect(header.to).to(contain("alice@dev.pixelated-project.org"))
+                expect(header.cc).to(beNil())
+                expect(header.bcc).to(beNil())
+                expect(header.subject) == "Welcome"
+                expectParsed(header.date)
+                expect(mail.textPlainBody) == "First mail"
+                expect(mail.mailbox) == "inbox"
+                expect(mail.ident) == "M-xxxb"
             }
         }
     }
